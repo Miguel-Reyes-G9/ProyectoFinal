c0c2360f8e410f03e9673f7695b0df19b87fe639
Miguel-Reyes-G9 <miguel.alberto.reyes.morales@gmail.com>
2026-05-17 16:43:06 -0600

    Update Examen.c

--- Examen.c
+++ Examen.c
@@ -1,92 +1,200 @@
-// PROYECTO FINAL 
+// PROYECTO FINAL
 // ALUMNO: REYES MORALES MIGUEL ALBERTO
 // MATERIA: FUNDAMENTOS DE PROGRAMACION
-# include <stdio.h>
+
+#include <stdio.h>
+
 int vect[10][100];
 int ant[2];
-int i, j, n, op, con, cont, puntaje, k;
+int pcont[10];
+int topoyent[100];
+int puntper[100];
 
-void main (){
-  for (j=0; j<100; j++) {
-    for (i=0; i<10; i++)
-      vect[i][j]=0; }
-  j=0, i=0, n=0, ant[0]=0, ant[1]=0, k=0;;
-  do {
-  printf ("Hola, oyente numero %d, que canciones pones en tu top 3?\n", n+1);
-  printf ("1. Cancion uno.\n");
-  printf ("2. Cancion dos.\n");
-  printf ("3. Cancion tres.\n");
-  printf ("4. Cancion cuatro.\n");
-  printf ("5. Cancion cinco.\n");
-  printf ("6. Cancion seis.\n");
-  printf ("7. Cancion siete.\n");
-  printf ("8. Cancion siete.\n");
-  printf ("9. Cancion siete.\n");
-  printf ("10. Cancion siete.\n");
-  printf ("Coloca el valor de menos uno para pasar a los resultados\n");
+int i, j, n, op, cont, puntaje, k, top;
 
-  for (puntaje=3; puntaje>0; puntaje--)
-    {
-    cont=0, con=0;
-    printf ("Cual es la cancion que para ti esta en el top %d ?\n", cont+1 );
-  
-do {
-    scanf ("%d", &op);
-    if (op == ant[0] && op==ant[0])
-      printf ("No puedes dar el mismo valor al anterior\n");
-    else {
-      if (op == -1)
-            con=1;
-        else {
-            if (op>=1 && op<=10)
-              cont=1;
-            else 
-              printf ("Dame un valor del 1 al 10\n"); }
-    }
-      } while (cont != 1 && con!= 1); 
+void inicializar ();
+void leerVotos ();
+void calcularCanciones ();
+void calcularGanador ();
+
+int main () {
+
+    inicializar();
+
+    leerVotos();
+
+    calcularCanciones();
+
+    calcularGanador();
+
+    return 0;
 }
-    ant[k]=op;
-    ++k;
-  
-    switch (op) {
-    case 1:
-      vect[0][j]=puntaje;
-      break;
-    case 2:
-      vect[1][j]=puntaje;
-      break;
-    case 3:
-      vect[2][j]=puntaje;
-      break;
-    case 4:
-      vect[3][j]=puntaje;
-      break;
-    case 5:
-      vect[4][j]=puntaje;
-      break;
-    case 6:
-      vect[5][j]=puntaje;
-      break;
-    case 7:
-      vect[6][j]=puntaje;
-      break;
-    case 8:
-      vect[7][j]=puntaje;
-      break;
-    case 9:
-      vect[8][j]=puntaje;
-      break;
-    case 10:
-      vect[9][j]=puntaje;
-      break;
-      }
+
+void inicializar () {
+
+    for (j=0; j<100; j++) {
+        for (i=0; i<10; i++)
+            vect[i][j]=0;
     }
-    k=0;
-    ++j;
- } while (con != 1);
-// BORRAR 
-  for (j=0; j<100; j++) {
+
     for (i=0; i<10; i++)
-      printf ("%d ,", vect[i][j]);
-  printf ("\n");}
+        pcont[i]=0;
+
+    j=0;
+    n=0;
+    ant[0]=47667;
+    ant[1]=32432;
+    top=1;
+}
+
+void leerVotos () {
+
+    do {
+
+        printf ("Hola, oyente numero %d, que canciones pones en tu top?\n", j);
+
+        printf ("0. Cancion cero.\n");
+        printf ("1. Cancion uno.\n");
+        printf ("2. Cancion dos.\n");
+        printf ("3. Cancion tres.\n");
+        printf ("4. Cancion cuatro.\n");
+        printf ("5. Cancion cinco.\n");
+        printf ("6. Cancion seis.\n");
+        printf ("7. Cancion siete.\n");
+        printf ("8. Cancion ocho.\n");
+        printf ("9. Cancion nueve.\n");
+        printf ("-1. Resultados.\n");
+
+        for (puntaje=3; puntaje>0; puntaje--) {
+
+            cont=0;
+
+            printf ("Cual es la cancion que para ti esta en el top %d ?\n", top);
+
+            do {
+
+                scanf ("%d", &op);
+
+                if (op == ant[0] || op == ant[1])
+                    printf ("No puedes repetir canciones.\n");
+
+                else {
+
+                    if (op>=0 && op<=9)
+                        cont=1;
+
+                    else if (op != -1)
+                        printf ("Dame un valor del 0 al 9\n");
+                }
+
+            } while (cont != 1 && op != -1);
+
+            if (op == -1)
+                break;
+
+            ant[3-puntaje]=op;
+
+            vect[op][j]=puntaje;
+
+            ++top;
+        }
+
+        top=1;
+
+        ant[0]=47667;
+        ant[1]=32432;
+
+        ++j;
+
+    } while (j<100 && op!=-1);
+
+    --j;
+
+    for (i=0; i<10; i++)
+        vect[i][j]=0;
+}
+
+void calcularCanciones () {
+
+    for (cont=0; cont<j; cont++) {
+
+        for (i=0; i<10; i++) {
+
+            pcont[i]+=vect[i][cont];
+        }
+    }
+
+    printf ("\n");
+
+    for (i=0; i<10; i++)
+        printf ("Cancion %d: %d votos\n", i, pcont[i]);
+
+    k=0;
+
+    for (i=0; i<10; i++) {
+
+        if (pcont[i] > pcont[k])
+            k=i;
+    }
+
+    printf ("\n1a cancion: %d\n", k);
+
+    n=0;
+
+    for (i=0; i<10; i++) {
+
+        if (i != k) {
+
+            if (pcont[i] > pcont[n] || n == k)
+                n=i;
+        }
+    }
+
+    printf ("2a cancion: %d\n\n", n);
+}
+
+void calcularGanador () {
+
+    for (cont=0; cont<j; cont++) {
+
+        puntper[cont]=0;
+
+        if (vect[k][cont]!=0)
+            puntper[cont]+=30;
+
+        if (vect[n][cont]!=0)
+            puntper[cont]+=20;
+
+        if ((vect[k][cont]!=0) && (vect[n][cont]!=0))
+            puntper[cont]+=10;
+    }
+
+    for (i=0; i<j; i++)
+        printf ("Oyente %d: %d puntos\n", i, puntper[i]);
+
+    k=0;
+
+    for (i=0; i<j; i++) {
+
+        if (puntper[i] > puntper[k])
+            k=i;
+    }
+
+    n=0;
+
+    for (i=0; i<j; i++) {
+
+        if (puntper[i] == puntper[k]) {
+
+            topoyent[n]=i;
+            ++n;
+        }
+    }
+
+    printf ("\nGanador: ");
+
+    for (cont=0; cont<n; cont++)
+        printf ("Oyente numero %d ", topoyent[cont]);
+
+    printf ("\n");
 }
